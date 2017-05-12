69f849308aa212264f6fe7cbc652703d440662c3
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-12 10:49:13 +0100

    Buttons hooked up to javascript methods

--- jasmine/controller.js
+++ jasmine/controller.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
 
-var thermostat = new Thermostat();
+  var thermostat = new Thermostat();
 
-$('#temperature').text(thermostat.temperature);
-$('#power_saving').text(thermostat.powerSaving);
-$('#power_usage').text(thermostat.currentUsage());
-
-$('#increase_temp').on('click', function() {
-  thermostat.up();
   $('#temperature').text(thermostat.temperature);
-})
+  $('#power_saving').text(thermostat.powerSaving);
+  $('#power_usage').text(thermostat.currentUsage());
 
+  $('#increase_temp').on('click', function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+  })
+
+  $('#reduce_temp').on('click', function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+
+  $('#reset_temp').on('click', function() {
+    thermostat.reset();
+    $('#temperature').text(thermostat.temperature);
+  });
+
+  $('#toggle_saving').on('click', function() {
+    if(thermostat.powerSaving == true) {
+      thermostat.powerSaving = false
+    } else if(thermostat.powerSaving == false) {
+      thermostat.powerSaving = true
+    };
+  });
 });
